c89c19ae54f7eabf07c3381b5829fc2e7f9b094d
Najib Alejandro <alejandrorod1498@gmail.com>
2025-02-24 15:46:41 -0600

    fix reload

--- app/api/v1/endpoints.py
+++ app/api/v1/endpoints.py
@@ -14,3 +14,7 @@ def create_user_endpoint(user: UserCreate, db: Session = Depends(get_db)):
 @router.get("/users/", response_model=List[UserOut])
 def get_users_endpoint(db: Session = Depends(get_db)):
     return list_users(db)
+
+@router.get("/some/")
+def get_users_endpoint():
+    return {"message": "test parece que tenia razon alv siuu "}
